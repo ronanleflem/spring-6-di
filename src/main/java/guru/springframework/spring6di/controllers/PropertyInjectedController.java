5bf041f33228135a042d60f997e7d7d924701dc1
ronanleflem <ronan.leflem@gmail.com>
2024-08-07 18:02:16 +0200

    using qualifiers

--- src/main/java/guru/springframework/spring6di/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/PropertyInjectedController.java
@@ -3,11 +3,12 @@ package guru.springframework.spring6di.controllers;
 import guru.springframework.spring6di.services.GreetingService;
 import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
-
+    @Qualifier("propertyGreetingService")
     @Autowired
     GreetingService greetingService;
 
